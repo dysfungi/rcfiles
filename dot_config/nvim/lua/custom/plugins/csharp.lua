a14882ee947418e4d80f523a30db87bacf81e68b
Derek M Frank <derek@frank.sh>
2025-01-12 17:57:50 -0800

    nvim: Enable Kickstart, F#, and C# plugins

---
+++ dot_config/nvim/lua/custom/plugins/csharp.lua
@@ -0,0 +1,3 @@
+return {
+  'iabdelkareem/csharp.nvim',
+}
---
+++ dot_config/nvim/lua/custom/plugins/fsharp.lua
@@ -0,0 +1,5 @@
+-- https://github.com/ionide/Ionide-vim/issues/84#issuecomment-2154200542
+return {
+  'ionide/Ionide-vim',
+  -- event = 'LazyFile',
+}
