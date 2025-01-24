c2b8a31e387e182bcbaaf693660b8023396cd2e8
Derek M Frank <dfrank@riotgames.com>
2025-01-24 11:24:42 -0800

    fix(nvim): Specify pets.nvim dependencies and setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1100,7 +1100,10 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
-  'giusgad/pets.nvim',
+  {
+    'giusgad/pets.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'giusgad/hologram.nvim' },
+  },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -1127,6 +1130,15 @@ require('lazy').setup({
     },
   },
 })
+
+require('hologram').setup {
+  auto_display = true,
+}
+require('pets').setup {
+  default_pet = 'dog',
+  default_style = 'black',
+  random = false,
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
