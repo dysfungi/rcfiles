73dc05c9f076fe51d9036d151516382a2b374361
Derek M Frank <dfrank@riotgames.com>
2025-03-04 17:04:25 -0800

    refactor(nvim): Add default mason-lspconfig options

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -808,6 +808,8 @@ require('lazy').setup({
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
+        automatic_installation = true,
+        ensure_installed = ensure_installed,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
