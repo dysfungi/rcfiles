96d7e209a43ab51c78edbefd94149d8e46cb3d90
Derek M Frank <derek@frank.sh>
2025-06-19 15:10:08 -0700

    fix(scripts): Remove nvim MasonToolsClean

--- run_after_I-neovim-mason-update-sync.sh
+++ run_after_I-neovim-mason-update-sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-nvim --headless +MasonUpdate +MasonToolsClean +MasonToolsInstallSync +MasonToolsUpdateSync +q
+nvim --headless +MasonUpdate +MasonToolsInstallSync +MasonToolsUpdateSync +q
