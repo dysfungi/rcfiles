ef4cd2a8fbb50527aca732d1a8450292f981550b
Derek M Frank <dfrank@riotgames.com>
2025-06-27 09:31:27 -0700

    feat(nvim): Add Markdown renderer plugin

--- dot_config/nvim/lua/plugins/languages/markdown.lua
+++ dot_config/nvim/lua/plugins/languages/markdown.lua
@@ -1,20 +1,26 @@
--- install without yarn or npm
--- return {
---   'iamcco/markdown-preview.nvim',
---   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
---   ft = { 'markdown' },
---   build = function()
---     vim.fn['mkdp#util#install']()
---   end,
--- }
-
--- install with yarn or npm
 return {
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && yarn install',
-  init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
-  ft = { 'markdown' },
+  {
+    -- install with yarn or npm
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+    -- install without yarn or npm
+    -- 'iamcco/markdown-preview.nvim',
+    -- cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    -- ft = { 'markdown' },
+    -- build = function() vim.fn['mkdp#util#install']() end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
