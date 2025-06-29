26e33f4a4209097d1e5de3d07562ca18ce1b1291
Derek M Frank <derek@frank.sh>
2025-06-29 13:33:30 -0700

    feat(nvim): Implement my own random choice of pet

--- dot_config/nvim/after/plugin/pets.lua
+++ dot_config/nvim/after/plugin/pets.lua
@@ -1,3 +1,15 @@
+local choices = {
+  'dog black Dino',
+  'cockatiel gray Cock',
+  'crab red Crusty',
+  'mod purple dotnet-bot',
+  'rocky gray Skip',
+  'rubber-duck yellow Ducky',
+}
+math.randomseed(os.time())
+local choice = choices[math.random(#choices)]
+
 if package.loaded['pets'] then
-  vim.cmd.PetsNew 'Dino'
+  -- vim.cmd.PetsNew 'Dino'
+  vim.cmd.PetsNewCustom(choice)
 end
