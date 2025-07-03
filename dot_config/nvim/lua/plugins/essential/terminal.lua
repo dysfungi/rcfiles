a5e3eb23cb8019f0f74e3107b1d3d08daa266258
Derek M Frank <derek@frank.sh>
2025-07-03 14:13:39 -0700

    fix(nvim): Stop insert when opening new Neovim

--- dot_config/nvim/lua/plugins/essential/terminal.lua
+++ dot_config/nvim/lua/plugins/essential/terminal.lua
@@ -2,15 +2,29 @@
 vim.api.nvim_create_autocmd('VimEnter', {
   desc = 'Auto-open a terminal',
   pattern = '*',
-  command = 'vsplit term://xonsh',
+  -- command = 'vsplit term://xonsh',
+  callback = function(ev)
+    vim.cmd 'vsplit term://xonsh'
+    vim.cmd 'wincmd h'
+    vim.cmd 'stopinsert'
+  end,
+  -- callback = function(ev)
+  --   local win_id = vim.api.nvim_open_win(0, false, {
+  --     split = 'right',
+  --   })
+  --   local buf_id = vim.fn.winbufnr(win_id)
+  --   vim.api.nvim_win_call(win_id, function()
+  --     vim.cmd 'edit! term://xonsh'
+  --   end)
+  -- end,
   nested = true,
 })
 
 -- https://github.com/rebelot/terminal.nvim#auto-insert-mode
 -- https://github.com/neovim/neovim/issues/2815#issuecomment-110571245
 vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter', 'TermOpen' }, {
-  callback = function(args)
-    if vim.startswith(vim.api.nvim_buf_get_name(args.buf), 'term://') then
+  callback = function(ev)
+    if vim.startswith(vim.api.nvim_buf_get_name(ev.buf), 'term://') then
       vim.cmd 'startinsert'
     end
   end,
