ef6d547b58dc88ce80a9ec36bc1536a6283705f2
Derek M Frank <derek@frank.sh>
2025-07-03 12:41:12 -0700

    feat(nvim): Auto-open terminal vertically and enter insert mode by default

--- dot_config/nvim/lua/plugins/essential/terminal.lua
+++ dot_config/nvim/lua/plugins/essential/terminal.lua
@@ -1,8 +1,19 @@
+-- https://neovim.io/doc/user/terminal.html#_start
 vim.api.nvim_create_autocmd('VimEnter', {
   desc = 'Auto-open a terminal',
   pattern = '*',
-  command = 'split term://xonsh',
+  command = 'vsplit term://xonsh',
   nested = true,
+})
+
+-- https://github.com/rebelot/terminal.nvim#auto-insert-mode
+-- https://github.com/neovim/neovim/issues/2815#issuecomment-110571245
+vim.api.nvim_create_autocmd({ 'WinEnter', 'BufWinEnter', 'TermOpen' }, {
+  callback = function(args)
+    if vim.startswith(vim.api.nvim_buf_get_name(args.buf), 'term://') then
+      vim.cmd 'startinsert'
+    end
+  end,
 })
 
 return {
