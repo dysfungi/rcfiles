e5aa87a888661489e7cc90166ccfa3ec6df0eae0
Derek M Frank <derek@frank.sh>
2025-07-03 14:26:07 -0700

    fix(nvim): Disable auto-opening split terminal due to annoyances and git commit editing

--- dot_config/nvim/lua/plugins/essential/terminal.lua
+++ dot_config/nvim/lua/plugins/essential/terminal.lua
@@ -1,24 +1,15 @@
 -- https://neovim.io/doc/user/terminal.html#_start
-vim.api.nvim_create_autocmd('VimEnter', {
-  desc = 'Auto-open a terminal',
-  pattern = '*',
-  -- command = 'vsplit term://xonsh',
-  callback = function(ev)
-    vim.cmd 'vsplit term://xonsh'
-    vim.cmd 'wincmd h'
-    vim.cmd 'stopinsert'
-  end,
-  -- callback = function(ev)
-  --   local win_id = vim.api.nvim_open_win(0, false, {
-  --     split = 'right',
-  --   })
-  --   local buf_id = vim.fn.winbufnr(win_id)
-  --   vim.api.nvim_win_call(win_id, function()
-  --     vim.cmd 'edit! term://xonsh'
-  --   end)
-  -- end,
-  nested = true,
-})
+-- vim.api.nvim_create_autocmd('VimEnter', {
+--   desc = 'Auto-open a terminal',
+--   pattern = '*',
+--   -- command = 'vsplit term://xonsh',
+--   callback = function(ev)
+--     vim.cmd 'vsplit term://xonsh'
+--     vim.cmd 'wincmd h'
+--     vim.cmd 'stopinsert'
+--   end,
+--   nested = true,
+-- })
 
 -- https://github.com/rebelot/terminal.nvim#auto-insert-mode
 -- https://github.com/neovim/neovim/issues/2815#issuecomment-110571245
