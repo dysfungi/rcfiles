9133c709925d7cfeae5bfdb8445bad22acada083
Derek M Frank <derek@frank.sh>
2025-07-07 13:46:23 -0700

    chore(scripts): Redirect stdout to temp files

--- run_after_I-neovim-sync.sh
+++ run_after_I-neovim-sync.sh
@@ -8,8 +8,12 @@ else
   echo >&2 "INFO: Starting $0"
 fi
 
+scriptName="$(basename $0)"
+outFile="$(mktemp -t "${scriptName}.out")"
+echo >&2 "INFO: Stdout file for ${scriptName} - ${outFile}"
+
 # https://lazy.folke.io/usage
-nvim --headless "+MasonUpdate" "+MasonToolsInstallSync" "+MasonToolsUpdateSync" "+Lazy! sync" "+qa"
+nvim --headless "+MasonUpdate" "+MasonToolsInstallSync" "+MasonToolsUpdateSync" "+Lazy! sync" "+qa" >> "${outFile}"
 
 echo >&2 "INFO: Ending $0"
 echo >&2
