6aa491d400ca6073a08c106ae061ebba66e5e234
Derek M Frank <derek@frank.sh>
2025-07-11 12:43:25 -0700

    chore(nvim): Enable focus.nvim

--- dot_config/nvim/lua/plugins/essential/empty_auto-resize.lua
+++ dot_config/nvim/lua/plugins/essential/empty_auto-resize.lua
@@ -1,4 +1,16 @@
 return {
   'nvim-focus/focus.nvim',
   version = '*',
+  opts = {
+    -- https://github.com/nvim-focus/focus.nvim#configuration
+    enable = true,
+    commands = true,
+    autoresize = {
+      enable = true,
+    },
+    split = {
+      bufnew = false,
+      tmux = false,
+    },
+  },
 }
