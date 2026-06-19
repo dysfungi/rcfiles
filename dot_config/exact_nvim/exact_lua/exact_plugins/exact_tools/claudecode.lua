1971467543c5d47e1a95efcaa7cc7923f9be8ae6
Derek M Frank <dfrank@riotgames.com>
2026-06-18 19:34:21 -0700

    chore(nvim): Configure claudecode.nvim to allow dangerously skip permissions

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_tools/claudecode.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_tools/claudecode.lua
@@ -13,4 +13,7 @@ return {
     { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
     { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
   },
+  opts = {
+    terminal_cmd = "claude --allow-dangerously-skip-permissions",
+  },
 }
